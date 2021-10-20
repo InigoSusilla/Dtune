be990c89977b01c7a0d8745221b102c44c0980cb
susil <susil@10.166.60.94>
2021-10-20 17:09:04 +0200

    Subida 1

--- DTune/src/VentanaPreLogging.java
+++ DTune/src/VentanaPreLogging.java
@@ -4,6 +4,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
 
 public class VentanaPreLogging extends JFrame {
 
@@ -35,8 +37,11 @@ public class VentanaPreLogging extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		contentPane.add(lblNewLabel);
 	}
 
 }
