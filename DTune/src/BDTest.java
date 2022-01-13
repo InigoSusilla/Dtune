a3a927cab984eee94d700de746189783dfb70e9a
jbarr <jbarr@LAPTOP-G7IPSRI5.home>
2022-01-13 22:23:42 +0100

    JUnit - Test Base de datos

--- DTune/src/BDTest.java
+++ DTune/src/BDTest.java
@@ -1,40 +1,37 @@
+import static org.junit.Assert.*;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-
-import java.sql.Time;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.Random;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 public class BDTest {
 
 	@Test
-	void testInsertarUsuario() {
-		Usuario u1 = new Cliente("u","y");
+	public void testInsertarUsuario() {
+		Usuario u1 = new Administrador("u","y");
 		BaseDeDatos.insertarUsuario(u1);
 		Usuario u2 = BaseDeDatos.mostrarUsuario("u");
-		assertNotNull(u2);
-		assertEquals("y", u2.getContrasenia());
+		assertTrue(u2 != null);
+		
 	}
 	
 	@Test
-	void testInsertarCancion() throws ParseException {
+	public void testBorrarCancion() throws ParseException {
 		SimpleDateFormat sdf = new SimpleDateFormat( "dd/MM/yyyy" );
 		Date fecha = sdf.parse("12/02/2001");
-		Cancion c1 = new Cancion("n","a",1,true, fecha, "Pop","ss",1.3);
+		Cancion c1 = new Cancion("Prueba","a",1,true, fecha, "Pop","ss",1.3);
 		BaseDeDatos.insertarCancion(c1);
-		ArrayList<Cancion> ar = BaseDeDatos.obtenerCanciones();
-		assertNotNull(ar);
-		assertEquals(c1, ar.get(0));
+		ArrayList<Cancion> canciones = BaseDeDatos.obtenerCanciones();
+		BaseDeDatos.eliminarCancion("Prueba");
+		ArrayList<Cancion> canciones2 = BaseDeDatos.obtenerCanciones();
+		assertTrue(canciones.size() < canciones2.size()-1);
+		
 	}
 	
+	
+
+
 }
